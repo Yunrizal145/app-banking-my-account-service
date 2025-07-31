023ad55983aee27d9254cad6a4c6f3f046bf6596
Yunrizal27 <hammad.aushaf@ist.id>
2025-07-31 10:43:25 +0700

    fix code

--- src/main/java/com/spring/myaccountservice/controller/MyAccountServiceController.java
+++ src/main/java/com/spring/myaccountservice/controller/MyAccountServiceController.java
@@ -4,6 +4,7 @@ import com.spring.myaccountservice.dto.RequestListTransactionHistory;
 import com.spring.myaccountservice.service.MyAccountService;
 import com.spring.myaccountservice.service.UserManagementService;
 import com.spring.transactionhistorymanagementservice.dto.GetListTransactionHistoryResponse;
+import com.spring.usermanagementservice.dto.GetUserFavoriteResponse;
 import com.spring.usermanagementservice.dto.OtpRequest;
 import com.spring.usermanagementservice.dto.OtpValidationRequest;
 import com.spring.usermanagementservice.dto.OtpValidationResponse;
@@ -48,5 +49,10 @@ public class MyAccountServiceController {
     @PostMapping("/getlisttransactionhistory")
     public GetListTransactionHistoryResponse getListTransactionHistoryResponse(@RequestBody RequestListTransactionHistory request) {
         return myAccountService.getListTransactionHistoryResponse(request);
+    }
+
+    @PostMapping("/getuserfavorite")
+    public GetUserFavoriteResponse getUserFavorite(@RequestBody RequestListTransactionHistory request) {
+        return myAccountService.getuserFavorite(request);
     }
 }
